4930d106b2bcecfd83f1c25619665a6eda831536
Alifer Santana <aliferass@hotmail.com.br>
2021-06-23 18:08:32 -0300

    first commit

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,86 @@
+import React, { useState, useCallback, useEffect } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useFocusEffect } from '@react-navigation/native';
+
+import { SearchBar } from '../../components/SearchBar';
+import { LoginDataItem } from '../../components/LoginDataItem';
+
+import {
+  Container,
+  LoginList,
+  EmptyListContainer,
+  EmptyListMessage
+} from './styles';
+
+interface LoginDataProps {
+  id: string;
+  title: string;
+  email: string;
+  password: string;
+};
+
+type LoginListDataProps = LoginDataProps[];
+
+export function Home() {
+   const [searchListData, setSearchListData] = useState<LoginListDataProps>([]);
+   const [data, setData] = useState<LoginListDataProps>([]);
+
+   const Key = '@passmanager:logins';
+
+  async function loadData() {
+    const data = await AsyncStorage.getItem(Key);
+    
+
+    if(!data)
+    return;
+
+    setData(JSON.parse(data));
+    setSearchListData(JSON.parse(data));
+  
+  }
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  useFocusEffect(useCallback(() => {
+    loadData();
+  }, []));
+
+  function handleFilterLoginData(search: string) {
+
+    if(search !== ''){
+      const searchFilter = data.filter(item => item.title === search);
+
+      setSearchListData(searchFilter);
+    }else {
+      setSearchListData(data);
+    }
+  }
+
+  return (
+    <Container>
+      <SearchBar
+        placeholder="Pesquise pelo nome do serviço"
+        onChangeText={(value) => handleFilterLoginData(value)}
+      />
+
+      <LoginList
+        keyExtractor={(item) => item.id}
+        data={searchListData}
+        ListEmptyComponent={(
+          <EmptyListContainer>
+            <EmptyListMessage>Nenhum item a ser mostrado</EmptyListMessage>
+          </EmptyListContainer>
+        )}
+        renderItem={({ item: loginData }) => {
+          return <LoginDataItem
+            title={loginData.title}
+            email={loginData.email}
+            password={loginData.password}
+          />
+        }}
+      />
+    </Container>
+  )
+}
